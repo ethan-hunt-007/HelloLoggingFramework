0255fd92f4a1843b73da262c72cf7eee26fdb309
Jayant Jaiswal <jayant.jaiswal@grabtaxi.com>
2020-04-12 00:22:34 +0530

    added changes

---
+++ HelloLoggingFramework.framework/Headers/HelloLoggingFramework.h
@@ -0,0 +1,19 @@
+//
+//  HelloLoggingFramework.h
+//  HelloLoggingFramework
+//
+//  Created by Jayant Jaiswal on 09/04/20.
+//  Copyright © 2020 GrabTaxi Pte Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloLoggingFramework.
+FOUNDATION_EXPORT double HelloLoggingFrameworkVersionNumber;
+
+//! Project version string for HelloLoggingFramework.
+FOUNDATION_EXPORT const unsigned char HelloLoggingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloLoggingFramework/PublicHeader.h>
+
+
